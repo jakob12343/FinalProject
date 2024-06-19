5d49395b27f8c405e393bf39c3bca81fdcefad26
Yakov Sabah <79536852+jakob12343@users.noreply.github.com>
2024-06-20 01:23:55 +0300

    Update DB.js
    ok

--- Server/DAL/DB.js
+++ Server/DAL/DB.js
@@ -5,7 +5,7 @@ const uploaddb = () => {
     const db = mongoose.connection
     const mongoUrl = `mongodb://localhost:27017/SuveySay`
     mongoose.connect(mongoUrl)
-    db.on('error', () => { console.log(error); })
+    db.on('error', (error) => { console.log(error); })
     db.once('connected', () => { console.log(`db is running`); })
 }
 module.exports = uploaddb
