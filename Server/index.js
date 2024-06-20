f274b3a9c9ce707d127418ecccee5bc0788911e2
Yakov Sabah <79536852+jakob12343@users.noreply.github.com>
2024-06-20 03:21:22 +0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,8 +7,15 @@ const Delete = require('./Controllers/Delete');
 const Update = require('./Controllers/Update');
 const Middlewares = require('./Controllers/MiddleWares');
 const DataBase = require('./DAL/DB');
-const cors = require('cors');
 require('dotenv').config();
+const corsOptions = {
+  origin: 'https://final-project-client-smoky.vercel.app', // Your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow credentials if needed
+  optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3000;
 const app = express();
