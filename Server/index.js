0db44e42c35506992cd7eb9acdec2eb337bc9b91
Yakov Sabah <79536852+jakob12343@users.noreply.github.com>
2024-06-20 03:44:44 +0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,46 +1,28 @@
 // requires
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const Get = require('./Controllers/Read');
 const Create = require('./Controllers/Create');
 const Delete = require('./Controllers/Delete');
 const Update = require('./Controllers/Update');
 const Middlewares = require('./Controllers/MiddleWares');
 const DataBase = require('./DAL/DB');
+const cors = require('cors');
 require('dotenv').config();
 
+const port = process.env.PORT || 3000;
 const app = express();
-
-// CORS options to allow any origin
-const corsOptions = {
-  origin: '*', // Allow any origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow credentials if needed
-  optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-// Use CORS with options
-app.use(cors(corsOptions));
-
-// Middleware to add headers manually to allow any origin
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  console.log('CORS headers set for', req.path);
-  next();
-});
-
-// Handle preflight requests
-app.options('*', cors(corsOptions));
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 // activate DB
 DataBase();
 
-// CRUD Operations
+
+
+//////////////// CRUD////////////////
 // Posts
 app.post('/Register', (req, res) => { Create.Register(req, res); });
 app.post('/PublishSuervey', (req, res) => { Create.PublishSuervey(req, res); });
@@ -55,16 +37,16 @@ app.get('/PullUserSurveys', (req, res) => { Get.PullUserSurveys(req, res); });
 app.get('/PullOldUserSurveys', (req, res) => { Get.PullOldUserSurveys(req, res); });
 app.get('/PullAllSurveys', (req, res) => { Get.PullAllSurveys(req, res); });
 app.post('/GetSurveys', (req, res) => { Create.GetSurveys(req, res); });
-app.get('/ReadPublicSurveys', (req, res) => { Get.ReadPublicSurveys(req, res); });
+app.get('/ReadPublicSurveys', (req,res)=>{Get.ReadPublicSurveys(req,res)})
 // Updates
 app.put('/UpdateUserDetails', (req, res) => { Update.UpdateUserDetails(req, res); });
 app.put('/Vote', (req, res) => { Update.Vote(req, res); });
 app.put('/EditPasword', (req, res) => { Update.EditPasword(req, res); });
 // Deletes
 app.delete('/DeletTargetSurvey', (req, res) => { Delete.DeleteSurvey(req, res); });
-app.get('/Check', (req, res) => { res.status(200).json({ message: "this is check from vercel" }); });
+app.get('/Check', (req,res)=>{res.status(200).json({messege: "this is check from vercel"})})
 
-const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
