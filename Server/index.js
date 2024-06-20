7a5a0184d13b451a351f7ad6f97cfd0f18b6a767
Yakov Sabah <79536852+jakob12343@users.noreply.github.com>
2024-06-20 03:54:45 +0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,13 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  console.log('CORS headers set for', req.path);
+  next();
+});
 // activate DB
 DataBase();
 
